66fd7d24ab8bb81bf2c183edcc96ac66de7366b2
doki <d0k1@focus-it.ru>
2014-12-16 23:59:18 +0500

    try to fix the bug

--- bond/src/main/java/com/focusit/agent/bond/Agent.java
+++ bond/src/main/java/com/focusit/agent/bond/Agent.java
@@ -4,14 +4,10 @@ import com.focusit.agent.bond.time.GlobalTime;
 import com.focusit.agent.metrics.JvmMonitoring;
 import com.focusit.agent.metrics.dump.SamplesDumpManager;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.jar.JarFile;
 import java.util.logging.Logger;
 
@@ -35,7 +31,6 @@ public class Agent
             agentInstrumentation = instrumentation;
 
             modifyBootstrapClasspathByArgs(agentArguments);
-            modifyBootstrapClasspath(instrumentation);
 
             if (!AgentConfiguration.isAgentEnabled())
             {
@@ -47,14 +42,7 @@ public class Agent
 
             startSensors();
 
-    	    LOG.info("!!Dumping");
-
             startDumping();
-
-	        LOG.info("!!Start transformer");
-
-            String excludes[] = AgentConfiguration.getExcludeClasses();
-            String ignoreExcludes[] = AgentConfiguration.getIgnoreExcludeClasses();
 
             AgentConfiguration.Transformer transformer = AgentConfiguration.getAgentClassTransformer();
 
@@ -75,7 +63,6 @@ public class Agent
                         new CGLibClassTransformer(excludes, ignoreExcludes, instrumentation), true);
             }
 
-    	    LOG.info("!!Retransform");
             retransformAlreadyLoadedClasses(instrumentation);
 
         }
@@ -134,66 +121,12 @@ public class Agent
         return false;
     }
 
-    private static void modifyBootstrapClasspath(Instrumentation instrumentation) throws IOException
-    {
-        //		if (AgentManager.agentJar != null) {
-        //			instrumentation.appendToBootstrapClassLoaderSearch(AgentManager.agentJar);
-        //		}
-        //
-        //		URLClassLoader loader = AgentManager.appClassloader;
-        //
-        //		if (loader == null)
-        //			loader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-        //
-        //		for (URL url : loader.getURLs()) {
-        //			if (AgentManager.agentJar == null && url.getFile().startsWith("bond") && url.getFile().endsWith(".jar")) {
-        //				instrumentation.appendToBootstrapClassLoaderSearch(new JarFile(url.getFile()));
-        //			}
-        //			if (url.getFile().contains("slf4j-api")) {
-        //				instrumentation.appendToBootstrapClassLoaderSearch(new JarFile(url.getFile()));
-        //			} else if (url.getFile().contains("slf4j-log4j12")) {
-        //				instrumentation.appendToBootstrapClassLoaderSearch(new JarFile(url.getFile()));
-        //			} else if (url.getFile().contains("log4j-")) {
-        //				instrumentation.appendToBootstrapClassLoaderSearch(new JarFile(url.getFile()));
-        //			} else if (url.getFile().contains("commons-lang3-")) {
-        //				instrumentation.appendToBootstrapClassLoaderSearch(new JarFile(url.getFile()));
-        //			} else if (url.getFile().contains("javassist")) {
-        //				instrumentation.appendToBootstrapClassLoaderSearch(new JarFile(url.getPath()));
-        //			}
-        //		}
-    }
-
     private static void modifyBootstrapClasspathByArgs(String agentArguments) throws IOException
     {
         if (System.getProperty("agent.jar") != null)
         {
-            //LOG.severe("Adding agent.jar to bootstrap classpath " + System.getProperty("agent.jar"));
             agentInstrumentation.appendToBootstrapClassLoaderSearch(new JarFile(System.getProperty("agent.jar")));
             agentInstrumentation.appendToSystemClassLoaderSearch(new JarFile(System.getProperty("agent.jar")));
-            //modifySystemClassloader(System.getProperty("agent.jar"));
-        }
-    }
-
-    private static void modifySystemClassloader(String... jars)
-    {
-        URLClassLoader sysloader = (URLClassLoader)ClassLoader.getSystemClassLoader();
-        Class sysclass = URLClassLoader.class;
-
-        Class[] parameters = new Class[] { URL.class };
-
-        try
-        {
-            Method method = sysclass.getDeclaredMethod("addURL", parameters);
-            method.setAccessible(true);
-            for (String jar : jars)
-            {
-                method.invoke(sysloader, new File(jar).toURL());
-            }
-        }
-        catch (Throwable e)
-        {
-            //LOG.severe("Error modifing system classloader: " + e.getMessage());
-            throw new RuntimeException(e);
         }
     }
 
@@ -212,7 +145,6 @@ public class Agent
                 {
                     try
                     {
-                        //LOG.info("Retransform class: "+cls.getName());
                         instrumentation.retransformClasses(cls);
                     }
                     catch (UnmodifiableClassException e)
@@ -221,8 +153,9 @@ public class Agent
                     }
                 }
             }
-        }catch(java.lang.Error e){
+        }catch(Throwable e){
             LOG.severe("Error retransforming class: " + e);
+            throw e;
         }
     }
 
@@ -239,23 +172,24 @@ public class Agent
             @Override
             public void run()
             {
-                try
-                {
-                    dataDumper.exit();
-                }
-                catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                }
+            try
+            {
+                dataDumper.exit();
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
 
-                try
-                {
-                    dataDumper.dumpRest();
-                }
-                catch (Throwable e)
-                {
-                    //LOG.severe("Shutdown hook error: " + e.getMessage());
-                }
+            try
+            {
+                dataDumper.dumpRest();
+            }
+            catch (Throwable e)
+            {
+                LOG.severe("Shutdown hook error: " + e.getMessage());
+                throw e;
+            }
             }
         });
     }
