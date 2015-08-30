d1b089d25d5b8383495d2a0fbd5dc03ade105dba
dOkI <denis.kirpichenkov@gmail.com>
2015-08-30 22:17:13 +0500

    updates

--- example/src/main/java/com/focusit/agent/example/example01/JavaAppExample03.java
+++ example/src/main/java/com/focusit/agent/example/example01/JavaAppExample03.java
@@ -1,5 +1,7 @@
 package com.focusit.agent.example.example01;
 
+import com.focusit.agent.example.example01.classes.*;
+
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,18 +17,132 @@ public class JavaAppExample03 {
 		Runnable work = new Runnable() {
 			@Override
 			public void run() {
-				try {
-					new ClassToInstrument().bar2();
-					new ClassToInstrument().bar2();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+				Class01 c01 = new Class01();
+				Class02 c02 = new Class02();
+				Class03 c03 = new Class03();
+				Class04 c04 = new Class04();
+				Class05 c05 = new Class05();
+				Class06 c06 = new Class06();
+				Class07 c07 = new Class07();
+				Class08 c08 = new Class08();
+				Class09 c09 = new Class09();
+				Class10 c10 = new Class10();
+
+				c01.m0((int) (Math.random() * 100));
+				c02.m1((int) (Math.random() * 100));
+				c03.m2((int) (Math.random() * 100));
+				c04.m3((int) (Math.random() * 100));
+				c05.m4((int) (Math.random() * 100));
+				c06.m5((int) (Math.random() * 100));
+				c07.m6((int) (Math.random() * 100));
+				c08.m7((int) (Math.random() * 100));
+				c09.m8((int) (Math.random() * 100));
+				c10.m9((int) (Math.random() * 100));
+
+				c01.m0((int) (Math.random() * 100));
+				c02.m1((int) (Math.random() * 100));
+				c03.m2((int) (Math.random() * 100));
+				c04.m3((int) (Math.random() * 100));
+				c05.m4((int) (Math.random() * 100));
+				c06.m5((int) (Math.random() * 100));
+				c07.m6((int) (Math.random() * 100));
+				c08.m7((int) (Math.random() * 100));
+				c09.m8((int) (Math.random() * 100));
+				c10.m9((int) (Math.random() * 100));
+
+				c01.m0((int) (Math.random() * 100));
+				c02.m1((int) (Math.random() * 100));
+				c03.m2((int) (Math.random() * 100));
+				c04.m3((int) (Math.random() * 100));
+				c05.m4((int) (Math.random() * 100));
+				c06.m5((int) (Math.random() * 100));
+				c07.m6((int) (Math.random() * 100));
+				c08.m7((int) (Math.random() * 100));
+				c09.m8((int) (Math.random() * 100));
+				c10.m9((int) (Math.random() * 100));
+
+				c01.m0((int) (Math.random() * 100));
+				c02.m1((int) (Math.random() * 100));
+				c03.m2((int) (Math.random() * 100));
+				c04.m3((int) (Math.random() * 100));
+				c05.m4((int) (Math.random() * 100));
+				c06.m5((int) (Math.random() * 100));
+				c07.m6((int) (Math.random() * 100));
+				c08.m7((int) (Math.random() * 100));
+				c09.m8((int) (Math.random() * 100));
+				c10.m9((int) (Math.random() * 100));
+
+				c01.m0((int) (Math.random() * 100));
+				c02.m1((int) (Math.random() * 100));
+				c03.m2((int) (Math.random() * 100));
+				c04.m3((int) (Math.random() * 100));
+				c05.m4((int) (Math.random() * 100));
+				c06.m5((int) (Math.random() * 100));
+				c07.m6((int) (Math.random() * 100));
+				c08.m7((int) (Math.random() * 100));
+				c09.m8((int) (Math.random() * 100));
+				c10.m9((int) (Math.random() * 100));
+
+				c01.m0((int) (Math.random() * 100));
+				c02.m1((int) (Math.random() * 100));
+				c03.m2((int) (Math.random() * 100));
+				c04.m3((int) (Math.random() * 100));
+				c05.m4((int) (Math.random() * 100));
+				c06.m5((int) (Math.random() * 100));
+				c07.m6((int) (Math.random() * 100));
+				c08.m7((int) (Math.random() * 100));
+				c09.m8((int) (Math.random() * 100));
+				c10.m9((int) (Math.random() * 100));
+
+				c01.m0((int) (Math.random() * 100));
+				c02.m1((int) (Math.random() * 100));
+				c03.m2((int) (Math.random() * 100));
+				c04.m3((int) (Math.random() * 100));
+				c05.m4((int) (Math.random() * 100));
+				c06.m5((int) (Math.random() * 100));
+				c07.m6((int) (Math.random() * 100));
+				c08.m7((int) (Math.random() * 100));
+				c09.m8((int) (Math.random() * 100));
+				c10.m9((int) (Math.random() * 100));
+
+				c01.m0((int) (Math.random() * 100));
+				c02.m1((int) (Math.random() * 100));
+				c03.m2((int) (Math.random() * 100));
+				c04.m3((int) (Math.random() * 100));
+				c05.m4((int) (Math.random() * 100));
+				c06.m5((int) (Math.random() * 100));
+				c07.m6((int) (Math.random() * 100));
+				c08.m7((int) (Math.random() * 100));
+				c09.m8((int) (Math.random() * 100));
+				c10.m9((int) (Math.random() * 100));
+
+				c01.m0((int) (Math.random() * 100));
+				c02.m1((int) (Math.random() * 100));
+				c03.m2((int) (Math.random() * 100));
+				c04.m3((int) (Math.random() * 100));
+				c05.m4((int) (Math.random() * 100));
+				c06.m5((int) (Math.random() * 100));
+				c07.m6((int) (Math.random() * 100));
+				c08.m7((int) (Math.random() * 100));
+				c09.m8((int) (Math.random() * 100));
+				c10.m9((int) (Math.random() * 100));
+
+				c01.m0((int) (Math.random() * 100));
+				c02.m1((int) (Math.random() * 100));
+				c03.m2((int) (Math.random() * 100));
+				c04.m3((int) (Math.random() * 100));
+				c05.m4((int) (Math.random() * 100));
+				c06.m5((int) (Math.random() * 100));
+				c07.m6((int) (Math.random() * 100));
+				c08.m7((int) (Math.random() * 100));
+				c09.m8((int) (Math.random() * 100));
+				c10.m9((int) (Math.random() * 100));
 			}
 		};
 
-		ExecutorService executor = Executors.newFixedThreadPool(100);
+		ExecutorService executor = Executors.newFixedThreadPool(5);
 
-		for(int i=0;i<10000000;i++){
+		for(int i=0;i<100000;i++){
 			executor.submit(work);
 		}
 		System.in.read();
---
+++ example/src/main/java/com/focusit/agent/example/example01/classes/Class08.java
@@ -0,0 +1,40 @@
+package com.focusit.agent.example.example01.classes;
+
+/**
+ * Created by Denis V. Kirpichenkov on 11.04.15.
+ */
+public class Class08 {
+	public int m0(int arg){
+		return 2*3*6*arg;
+	}
+	public int m1(int arg){
+		return 1+2*3*6*arg;
+	}
+	public int m2(int arg){
+		return 2*2*3*6*arg;
+	}
+	public int m3(int arg){
+		return 3+2*3*6*arg;
+	}
+	public int m4(int arg){
+		return 4*2*3*6*arg;
+	}
+	public int m5(int arg){
+		return 5+2*3*6*arg;
+	}
+	public int m6(int arg){
+		return 6*2*3*6*arg;
+	}
+	public int m7(int arg){
+		return 7+2*3*6*arg;
+	}
+	public int m8(int arg){
+		return 8*2*3*6*arg;
+	}
+	public int m9(int arg){
+		return 9+2*3*6*arg;
+	}
+	public int m10(int arg){
+		return 10*2*3*6*arg;
+	}
+}
